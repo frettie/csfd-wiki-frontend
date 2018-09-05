c74fb051430e7e3ce2dd31b641e95d8a13db4a70
frettie <frettie@gmail.com>
2018-09-05 18:20:43 +0200

    typo

--- justice-link.js
+++ justice-link.js
@@ -1,4 +1,4 @@
 jQuery(document).ready ( function() {
-    $('<span>&nbsp;<span><a target="_blank" href="https://or.justice.cz/ias/ui/rejstrik-$firma?nazev='+$('.wikibase-title-label').text()+'&typHledani=STARTS_WITH&jenPlatne=PLATNE">Justice.cz link</a>').insertAfter('.wikibase-title-id');
+    $('<span>&nbsp;<span><a target="_blank" href="https://or.justice.cz/ias/ui/rejstrik-$firma?nazev='+$('.wikibase-title-label').text().replace(" (Czechia)", "").replace('angličtina', '')+'&typHledani=STARTS_WITH&jenPlatne=PLATNE">Justice.cz link</a>').insertAfter('.wikibase-title-id');
 });
 
