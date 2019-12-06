a453641034b2a3bfe65a3592b4b627173e566d73
Jiri Sedlacek <jirisedlacek@gmail.com>
2019-12-06 09:35:12 +0100

    minus target blank

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-jQuery(document).ready ( function() {
-    $('<span>&nbsp;<span><a target="_blank" href="https://tools.wmflabs.org/csfd/film/'+$('#P2529 a.wb-external-id').text()+'">Quickstatements CSFD link</a>').insertAfter('#P2529 a.wb-external-id');
+jQuery(document).ready(function() {
+    $('<span>&nbsp;<span><a href="https://tools.wmflabs.org/csfd/film/' + $('#P2529 a.wb-external-id').text() + '">Quickstatements CSFD link</a>').insertAfter('#P2529 a.wb-external-id');
 });
-
-
