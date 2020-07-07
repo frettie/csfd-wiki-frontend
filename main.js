ff693ef3cc2819a6cfcd94f8c2cfd804abfeb691
frettie <frettie@gmail.com>
2020-07-08 00:51:27 +0200

    update main js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 jQuery(document).ready(function() {
-    $('<span>&nbsp;<span><a href="https://tools.wmflabs.org/csfd/film/' + $('#P2529 a.wb-external-id').text() + '">Quickstatements CSFD link</a>').insertAfter('#P2529 a.wb-external-id');
+    $('<span>&nbsp;<span><a href="https://csfd.toolforge.org/film/' + $('#P2529 a.wb-external-id').text() + '">Quickstatements CSFD link</a>').insertAfter('#P2529 a.wb-external-id');
 });
