555bf552cc5e965ea5ac23e0558bc3ef6817dabb
Jiri Sedlacek <jirisedlacek@gmail.com>
2026-01-01 16:12:28 +0100

    local fix

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 jQuery(document).ready(function() {
-    $('<span>&nbsp;<span><a href="https://csfd.toolforge.org/film/' + $('#P2529 a.wb-external-id').text() + '">Quickstatements CSFD link</a>').insertAfter('#P2529 a.wb-external-id');
+    $('<span>&nbsp;<span><a href="https://127.0.0.1:5000/film/' + $('#P2529 a.wb-external-id').text() + '">Quickstatements CSFD link</a>').insertAfter('#P2529 a.wb-external-id');
 });
